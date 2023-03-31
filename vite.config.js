f90bd5bb16716c7bcc454b3f7bd0c7cb458328d3
Carmen Lysla Ghirardi <carmenghirardi@gmail.com>
2023-03-31 11:17:53 +0200

    ok configurazione per eprj

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,17 @@
+import * as path from "path";
+import * as fs from "fs";
 import htmlPurge from "vite-plugin-html-purgecss";
+
+const files = fs.readdirSync("./");
+let array = [];
+files.forEach((file) => {
+  if (path.extname(file) === ".html") {
+    let fname = path.basename(file, path.extname(file));
+    let arrayEl = [fname, path.resolve(__dirname, file)];
+    array.push(arrayEl);
+  }
+});
+let object = Object.fromEntries(array);
 
 export default {
   server: {
@@ -8,5 +21,15 @@ export default {
   css: {
     devSourcemap: true,
   },
-  plugins: [htmlPurge({ safelist: [/show/, /hide/] })],
+  build: {
+    rollupOptions: {
+      input: object,
+      output: {
+        entryFileNames: "dist/[name].js",
+        chunkFileNames: "dist/[name].js",
+        assetFileNames: "dist/[name][extname]",
+      },
+    },
+  },
+  plugins: [htmlPurge({ safelist: [/show/, /hide/, /stick/, /slick/] })],
 };
