1b074b39556007ce22edc79ef520d2cefd72cb1c
Carmen Lysla Ghirardi <carmenghirardi@gmail.com>
2023-04-18 09:29:06 +0200

    ok base vite con fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,42 @@
-import "../scss/index.scss";
+import "./../scss/index.scss";
 
-import * as bootstrap from "bootstrap";
+import jquery from "jquery";
+window.jQuery = window.$ = jquery;
+
+// Bootstrap Framework
+import "@popperjs/core";
+import "bootstrap";
+
+import "slick-carousel";
+import hcSticky from "hc-sticky";
+
+//#region bootstrap navbar + hamburger + body scroll lock
+var navbarMain = document.getElementById("navbarMain");
+navbarMain.addEventListener("show.bs.collapse", function () {
+  $(".navbar-toggler .hamburger").addClass("is-active");
+  let scrollableNav = document.querySelector("#navbarMain");
+});
+navbarMain.addEventListener("hidden.bs.collapse", function () {
+  $(".navbar-toggler .hamburger").removeClass("is-active");
+  let scrollableNav = document.querySelector("#navbarMain");
+});
+//#endregion
+
+//#region stickybar
+document.addEventListener("DOMContentLoaded", function () {
+  new hcSticky(".stickybar", {
+    stickTo: "body",
+    stickyClass: "stickybar--sticked",
+  });
+});
+//#endregion
+
+//#region slider
+$(function () {
+  $(".slider").slick({
+    arrows: false,
+    centerMode: true,
+    variableWidth: true,
+  });
+});
+//#endregion
